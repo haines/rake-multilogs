882e49b8d9173ddfe760b820f419e3d1bf8d16f3
Andrew Haines <andrew@haines.org.nz>
2018-06-08 09:29:38 +0100

    Create gem

---
+++ test/rake/multilogs_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "../test_helper"
+
+module Rake
+  class MultilogsTest < Minitest::Test
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require "rake/multilogs"
+require "minitest/autorun"
